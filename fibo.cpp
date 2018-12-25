c2a3144dd1c6cc3e93ed5959e73bea7927e155c3
Harege <mamush.abay@gmail.com>
2018-12-25 12:31:29 -0500

    ver 01

--- fibo.cpp
+++ fibo.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 using namespace std;
+
 int main()
-{ 
-  cout <<"hello world" << endl;
+{
+  int a = -1, b = 1, c ,len = 0, count = 1;
+  cout << endl;
+  cout <<"       Fibonacci sequence generator" << endl;
+  cout <<"       Enter length of the sequence: ";
+  cin >> len;
+  cout <<"       ";
+  while(len)
+  {
+    c = a + b;
+    cout << c <<" ";
+    a = b;
+    b = c;
+    --len;
+    ++count;
+    if(!(count % 8)) cout << endl <<"       ";
+  }
+  cout << endl;
+  cout << endl;
   return 0;
 }
